5c75ce7b81bb188c26c60158468bc7a9622cd5b9
office10 <office10@ucione.local>
2024-04-24 17:43:45 +0200

    niz primjeri

---
+++ niz.js
@@ -0,0 +1,17 @@
+var brojcaniNiz = [1, 2, 3, 4, 5, 6, 7];
+//zbroji sve članove niza
+var suma = 0;
+for ( var x=0; x<brojcaniNiz.length; x++){
+suma += brojcaniNiz[x];
+}
+console.log('suma=' + suma);
+
+var matrix = [[1,2,3], ['John', 'Jane', 'Mike']];
+
+for (i = 0; i< matrix.length; i++){
+    console.log(matrix[i].join());
+    for (var j=0; j<matrix[i].length; j++){
+        console.log(matrix[i][j]);
+        
+    }
+}
